8627086a756b348ef8c2b4f7d3669c05f63b85f4
veeresh <higherthinkingquality@gmail.com>
2025-06-08 22:47:51 +0530

    stack and heap

--- datatype.js
+++ datatype.js
@@ -20,14 +20,45 @@ let myObj= {
   age : 24
 }
 
-const myFuntion = function (){
-   console.log("hello funtion")
+const myFunction = function (){
+   console.log("hello function")
 }
 
+console.log("-----------------------------------------------");
 
 console.log(typeof bigNumber);
 console.log(typeof heros);
 console.log(typeof myObj);
-console.log(typeof myFuntion);
+console.log(typeof myFunction);
+
+
+//--------------------------------------------------------
+console.log("-----------------------------------------------");
+
+//satck (priomitive) ,Heap (Non Primitive)
+
+let name= "youtube"
+let anotherName =name
+
+anotherName="instagram"
+
+console.log(name);
+console.log(anotherName);
+
+let user = {
+   email: "user@google.com",
+   upi : "user@ybl"
+}
+
+let usertwo = user
+
+//user varibale is in stack has the refrence of email and upi in heap
+// here usertwo has the refernce of user in heap
+
+usertwo.email="veer@gmail.com"
+
+console.log(user.email);
+console.log(usertwo.email); // here both print veer@gmail.com becuase both has  refernce of same email and upi in heap
+
 
 
