66bb13d7321344f54a11204ee933a2df1f28e497
veeresh <higherthinkingquality@gmail.com>
2025-07-08 00:32:22 +0530

    functib

--- 13_function.js
+++ 13_function.js
@@ -1,3 +1,26 @@
-if(2 =='2'){
- console.log("executed")
+if(2 =='2'){  // here it won't check datatype level both are equal
+ console.log("executed") // it prints as if is true
 }
+
+if(2==='2'){ // here it will check datatype so both are not same
+ console.log("executed ") // won't be executed
+}
+
+ // falsy values
+
+ // false , 0,-0,BigInt 0n , "",null , undefined ,NaN
+
+ // truthy values
+
+ // 'false', "0" ," ",[] , {} ,function () {}
+
+
+// nullish coalescing Operator  (??) : null undefined
+
+val1 = 5 ?? 10 // it will print 5
+val2 = null ?? 15 // it will print 15
+
+val3 =undefined ?? 78 // print 78
+
+// Terniary operator // this and above oeprator are different
+//condition ? true :false
